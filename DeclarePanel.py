34832a02f5423c9d321175ea9481c8d77ae971d4
Max ST <mstolpasov@gmail.com>
2019-03-23 19:09:24 +0300

    First

---
+++ DeclarePanel.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+# @Author: maxst
+# @Date:   2019-03-23 10:38:30
+# @Last Modified by:   maxst
+# @Last Modified time: 2019-03-23 18:36:53
+import sublime
+import sublime_plugin
+
+try:
+    from Anaconda.commands import AnacondaGoto as ParentResult
+except (ImportError):
+    ParentResult = sublime_plugin.TextCommand
+
+
+class BufferResult(object):
+    __slots__ = ('results', 'symbol', 'status')
+    __instance = None
+    START = 1
+    DONE = 2
+    HOLD = 3
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.clean()
+
+    def __str__(self):
+        return '{0} "{1}" {2}'.format('BufferResult', self.symbol, self.results)
+
+    def clean(self):
+        self.results = []
+        self.symbol = ''
+        self.status = self.HOLD
+
+    def is_loading(self):
+        return self.status == self.START
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = BufferResult()
+        return cls.__instance
+
+
+class GotoResult(ParentResult):
+    def run(self, edit, *args, **kwargs):
+        self.settings = sublime.load_settings("DeclarePanel.sublime-settings")
+        self.buffer = BufferResult.get_instance()
+        self.buffer.status = self.buffer.START
+        ignored_packages = self.view.settings().get('ignored_packages', [])
+
+        if all((
+                self.settings.get('use_anaconda', True),
+                hasattr(self, 'JEDI_COMMAND'),
+                'Anaconda' not in ignored_packages,
+        )):
+            super().run(edit)
+        else:
+            self.on_success({'result': None})
+
+    def on_success(self, data):
+        """Called when a result comes from the query
+        """
+        if not data.get('result'):
+            if self.buffer.symbol:
+                self.buffer.results = self.view.window().lookup_symbol_in_index(self.buffer.symbol)
+        else:
+            for result in data['result']:
+                path = self._infere_context_data(result[1])
+                self.buffer.results.append((path, path.split('/')[-1], (result[2], result[3])))
+                break
+        self.buffer.status = self.buffer.DONE
+
+
+class DeclarePanelCommand(sublime_plugin.WindowCommand):
+
+    NAME_PANEL = 'DeclarePanel'
+    NAME_REGION = 'region_declare'
+
+    def run(self, *args, **kwargs):
+        self.settings = sublime.load_settings("DeclarePanel.sublime-settings")
+        self.view = self.window.active_view()
+        if kwargs.get('show_panel', False):
+            return self.show_panel()
+        self.buffer = BufferResult.get_instance()
+        self.buffer.clean()
+        self.buffer.symbol = self.get_symbol()
+        self.search(self.buffer.symbol)
+
+    def get_symbol(self):
+        sel = self.view.sel()[0]
+        sel = self.view.word(sel) if sel.empty() else sel
+        return self.view.substr(sel).strip()
+
+    def search(self, symbol):
+        def async_search():
+            handle_results(self.buffer.results)
+
+        def handle_results(results):
+            if results:
+                self.print_symbol(results[0])
+            else:
+                sublime.status_message('Symbol "{0}" not found'.format(symbol))
+
+        self.window.run_command('goto_result')
+        self.run_after_loading(self.buffer, async_search)
+
+    def print_symbol(self, result):
+        source = ''
+        if result[0].lower() == self.view.file_name().lower():
+            source = self.view.substr(sublime.Region(0, self.view.size()))
+        else:
+            with open(result[0]) as file:
+                source = ''.join(file.readlines()).strip('\n')
+
+        self.print_to_panel(source, *result)
+
+    def description(self):
+        return 'DecPanel: Show declare\talt+s'
+
+    def run_after_loading(self, view, func):
+        """Run a function after the view has finished loading"""
+
+        def run():
+            if view.is_loading():
+                sublime.set_timeout(run, 10)
+            else:
+                # add an additional delay, because it might not be ready
+                # even if the loading function returns false
+                sublime.set_timeout(func, 10)
+
+        run()
+
+    def print_to_panel(self, text, path=None, file_name=None, line_col=(0, 0), region_mark=None):
+        self.kill_panel()
+        panel = self.window.create_output_panel(self.NAME_PANEL, False)
+        panel.set_read_only(False)
+        panel.run_command('append', {'characters': text})
+        panel.set_syntax_file(self.view.settings().get('syntax'))
+
+        def show_at_center():
+            panel.show_at_center(region_scrol)
+
+        if line_col[0] > 0:
+            point = panel.text_point(line_col[0] - 1, 1)
+            region_scrol = panel.line(point)
+            self.run_after_loading(panel, show_at_center)
+
+        if line_col:
+            region_mark = panel.word(panel.text_point(line_col[0] - 1, line_col[1]))
+            panel.add_regions(self.NAME_REGION, [region_mark], 'string', 'dot', sublime.DRAW_NO_FILL)
+
+        panel.set_read_only(True)
+        self.show_panel(panel)
+
+    def kill_panel(self):
+        self.window.destroy_output_panel(self.NAME_PANEL)
+
+    def show_panel(self, panel=None):
+        panel_view = self.window.find_output_panel(self.NAME_PANEL) if not panel else panel
+        if panel_view:
+            self.window.run_command('show_panel', {'panel': 'output.{0}'.format(self.NAME_PANEL), "toggle": True})
+            self.window.focus_view(panel_view)
+            if self.settings.get('scroll_toggle', True):
+                for reg in panel_view.get_regions(self.NAME_REGION):
+                    panel_view.show_at_center(reg)
+                    break
---
+++ README.md
@@ -0,0 +1,43 @@
+DeclarePanel
+=============
+
+Sometimes you just need to look at the implementation of the symbol and have to go to it.
+At the same time, we lose the context in which we were and if there were a lot of transitions,
+then it would be difficult to return to the initial place. DeclarePanel helps solve this problem.
+You can open the symbol implementation in the bottom panel without going to it.
+
+----
+
+Default settings:
+By default, we try to search through anaconda (can be disabled in the settings).
+We are looking for a second time through the standard Sublime Text mechanism.
+It always displays the first result.
+Be careful the first result may not be relevant.
+
+Usage
+-----
+
+In Sublime Text from the context menu 'Paste as. . . ' or hotkeys <kbd>Shift + Alt + v</kbd>
+Position on the desired character and hit <kbd>Alt + s</kbd>
+For toggle panel <kbd>Ctrl + Alt + s</kbd>
+### Screen
+![DeclarePanel]()
+
+
+Installation
+------------
+
+**Very easy with [Package Control](http://wbond.net/sublime_packages/package_control) right inside Sublime Text 3 (Package Control needs to be installed):**
+
+1.  <kbd>Ctrl + Shift + P</kbd>
+2.  Search for "install", hit enter
+3.  Search for "DeclarePanel", hit enter
+
+**Manually (not recommended):**
+
+1.  Clone or download this package
+2.  Put it into your Packages directory (find using 'Preferences' -> 'Browse Packages...')
+
+Credits
+-----
+[MOM](https://github.com/mom1)
